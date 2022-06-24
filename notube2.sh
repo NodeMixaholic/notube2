106cb3b368bb2bd76c5d3c8988207c881d471158
Samuel Lord <sam@sllord.info>
2022-06-23 23:29:20 -0500

    fricko broko

--- notube2.sh
+++ notube2.sh
@@ -1,2 +1,2 @@
-yt=$(curl "https://vid.puffyan.us/search?q=linux" | grep "youtube.com/watch?v=" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | head -n 1)
+yt=$(curl "https://vid.puffyan.us/search?q=$1" | grep "youtube.com/watch?v=" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort -u | head -n 1)
 mpv $yt
